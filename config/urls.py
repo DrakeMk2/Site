0648260d09e415d116f035fe235c45ec8c13de52
Drake <doosiv@gmail.com>
2026-07-14 17:41:42 +0300

    Q

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -25,3 +27,6 @@ urlpatterns = [
     path('calculator/', calculator_stub_view, name='calculator'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR / 'static')
